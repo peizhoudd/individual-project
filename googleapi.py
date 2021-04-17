8e6affadb96f7400637a1ec4633649b3d8c35074
peizhoudd <53030140+peizhoudd@users.noreply.github.com>
2021-04-17 14:24:10 +0800

    Update googleapi.py

--- googleapi.py
+++ googleapi.py
@@ -1,14 +1,14 @@
 import requests as req
 import re
-class GoogleDirect():
+class GoogleNavigation():
     def __init__(self):
         self.apikey="AIzaSyAni6-Fnvcn5lPL4ktloi3epdBYyHvP1aw"
 
     def getdirect(self,paramStarts,paramEnd):
-        ret_dic=req.post("https://maps.googleapis.com/maps/api/directions/json?origin="+paramStarts+"&destination="+paramEnd+"&key="+self.apikey).json()
+        dir=req.post("https://maps.googleapis.com/maps/api/directions/json?origin="+paramStarts+"&destination="+paramEnd+"&key="+self.apikey).json()
         htmlre=re.compile('>(.*?)<')
-        temp_steps=ret_dic["routes"][0]["legs"][0]["steps"]
+        i=dir["routes"][0]["legs"][0]["steps"]
         ret=""
-        for tem in temp_steps:
-            ret=ret+"---"+''.join(htmlre.findall(tem["html_instructions"]))
-        return ret
+        for tem in i:
+            rtn=rtn+"---"+''.join(htmlre.findall(tem["html_instructions"]))
+        return rtn
